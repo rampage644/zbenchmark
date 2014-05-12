rampage644/zbenchmark

commit abe62a5d1c075517dcea6a965327e562498e7205

Moved HEADing account to separate test
This is most simple test will help to test against internet/connections
and other non related to zerocloud middleware stuff.

swift.py
@@ -24,8 +24,8 @@ THREAD_COUNT = int(sys.argv[2]) if len(sys.argv) >= 3 else 50
 COUNT_PER_THREAD = OVERALL_COUNT / THREAD_COUNT
 
 
-def test(number):
-    """Zebra testing
+def test_post_single_job(number):
+    """Zebra JSON job POST testing
 
     :param number interger pointing to object to read ('object12.txt')
     Create new connection and post json job: print some object content
@@ -54,13 +54,28 @@ def test(number):
                       user=USER,
                       retries=1,
                       insecure=True)
-    conn.head_account()
     body, headers = conn.exec_account(
         None, None, data, content_type='application/json',
         content_length=len(data), headers={}, response_dict={})
 
 
-def test_batch(start, count):
+def test_head_account(number):
+    """Zebra simple HEAD account testing
+
+    :param number interger pointing to object to read ('object12.txt')
+    Create new connection and post json job: print some object content
+    to stdout"""
+
+    from zwiftclient.client import ZwiftConnection as Connection
+    conn = Connection(authurl=URL,
+                      key=KEY,
+                      user=USER,
+                      retries=1,
+                      insecure=True)
+    conn.head_account()
+
+
+def test_batch(start, count, func):
     """Running `test` function several times
 
     :param start integer to start with
@@ -71,7 +86,7 @@ def test_batch(start, count):
 
     for i in range(start, start + count + 1):
         try:
-            l = timeit.repeat(stmt='test(%d)' % i,
+            l = timeit.repeat(stmt='test_post_single_job(%d)' % i,
                               setup='from __main__ import test',
                               number=1, repeat=1)
             results.append(l[0])

commit 6ce667f997d0ed6b8ab0d3e27875979e6ac6fcb7

Fixed bug introduced in abe62a5d

swift.py
@@ -87,7 +87,7 @@ def test_batch(start, count, func):
     for i in range(start, start + count):
         try:
             l = timeit.repeat(stmt='test_post_single_job(%d)' % i,
-                              setup='from __main__ import test',
+                              setup='from __main__ import test_post_single_job',
                               number=1, repeat=1)
             results.append(l[0])
         except Exception as e: